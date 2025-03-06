691792504ed7672ef6f2e694ee3d8614126331fb
David Garcia De Mercado <involvedteacher.dev@gmail.com>
2025-03-06 16:27:39 +0100

    feat (netview): Add empty landing page.
    Add web server base code.
    Add empty landing page for "/" route.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"text/template"
+)
+
+var netview_path string = "../../internal/netview/"
+
+func indexHandler(rw http.ResponseWriter, r *http.Request) {
+	index_template, err := template.ParseFiles(netview_path + "index.html")
+	if err != nil {
+		panic(err)
+	} else {
+		index_template.Execute(rw, nil)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe("localhost:3000", nil)
+}
